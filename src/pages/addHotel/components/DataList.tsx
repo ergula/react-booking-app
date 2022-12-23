9b5e984cce56292aa60bdcb5cb8c4ae82a6280ae
aergul <alpyergul@gmail.com>
2022-12-23 14:36:21 +0300

    #19 Feat: Added DataTable Icons

--- src/pages/addHotel/components/DataList.tsx
+++ src/pages/addHotel/components/DataList.tsx
@@ -5,11 +5,18 @@ import { useNavigate } from "react-router-dom";
 import { confirmDialog } from "primereact/confirmdialog";
 import { Column } from "primereact/column";
 
+import  Delete from "../../../assets/images/delete.svg"
+import Add from "../../../assets/images/plus.svg";
+
 const DataList = () => {
   const headerBodyTemplate = (
     <div className="table-header">
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between">
         <h3 className="mx-2 my-4 text-xl text-black">Hotels</h3>
+        <div className="flex mt-2">
+          <img src={Add} alt="add" className="w-10 h-10 mr-4" />
+          <img src={Delete} alt="add" className="w-10 h-10 mr-4" />
+        </div>
       </div>
       <div className="custom-button-set">
         <img src="" className="custom-icon-button" />
@@ -28,7 +35,7 @@ const DataList = () => {
           rows={20}
           header={headerBodyTemplate}
         >
-            <Column field="invoice_type" header="Invoice Type" sortable></Column>
+          <Column field="invoice_type" header="Invoice Type" sortable></Column>
           <Column
             field="invoice_number"
             header="Invoice Number"
